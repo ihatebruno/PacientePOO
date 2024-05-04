0e03a1c08efe1e1f5ccfbf8a45b5c67a6dec69e7
brunogaelav2004@gmail.com <brunogaelav2004@gmail.com>
2024-05-04 13:43:07 -0600

    Version 2

--- Paciente.java
+++ Paciente.java
@@ -1,18 +1,36 @@
-public class Paciente{
-String nombre,apellido;
-int edad;
-double altura;
-
-public void mostrarNombreA() {
-    System.out.println("Mi nombre es:"+this.nombre+apellido);
-}
-    public void mostrarEdad() {
-
-        System.out.println("Mi edad es:"+this.edad);
+public class Paciente {
+    String nombre,apellido;
+    int edad;
+    double altura;
+    public String getNombre() {
+        return nombre;
     }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    @Override
+    public String toString() {
+        return "Persona [Nombre=" + nombre + ", Apellido=" + apellido + ", Edad=" + edad + ", Altura=" + altura + "]";
+    }
+    
+    
 
-public void mostrarAtura() {
-
-    System.out.println("Mi altura es:"+this.altura);
-}
 }
